8684a9d74e260383c55414a9c6334149fea61cf2
bpikaar <bpikaar@users.noreply.github.com>
2016-04-18 13:59:21 +0200

    php test

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <link rel="stylesheet" type="text/css" href="css/style.css">
   </head>
   <body>
+    <?php echo "Hello world!"; ?>
       <background></background>
       <fish></fish>
       <bubble></bubble>
